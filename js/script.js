a63508642ac10a993e2c0bcfb99776104761bc87
RANDERSON VIEIRA MENDES <17259228@CFP127ALUNO.LOCAL>
2018-09-19 11:30:14 -0300

    Fiz ajustes no layout, mas preciso resolver o footer

--- js/script.js
+++ js/script.js
@@ -96,7 +96,7 @@ function validar(caracter, blocktype, id) {
         else
             var letra = caracter.which;
                 
-        if(letra < 47 || letra > 57) {
+        if(letra != 8 && letra < 47 || letra > 57) {
             document.getElementById(id).style="background: #f00;";
             return false; /*Cancela a ação da tecla*/
         }  
